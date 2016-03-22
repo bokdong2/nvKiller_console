656a28f48852b95d1ee53702312bf5bdff9a68eb
PhilipMo <nodezero@naver.com>
2016-03-22 13:46:44 +0900

    nvKill
    Naver Antivirus Killer

---
+++ nvKiller_console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+namespace nvKiller_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                nvKill(Process.GetProcessesByName("nvcagent.npc"), Process.GetProcessesByName("nsvmon.npc"), Process.GetProcessesByName("nsavsvc.npc"));
+            }
+        }
+        static void nvKill(Process[] p1, Process[] p2, Process[] p3)
+        {
+            try
+            {
+                if ((p1.Length > 0 || p2.Length > 0) || p3.Length > 0)
+                {
+                    p1[0].Kill(); p2[0].Kill(); p3[0].Kill();
+                    Console.WriteLine("Killed");
+                }
+                else
+                {
+                    Console.WriteLine("Searching...");
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
